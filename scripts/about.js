e614bb80273b3b87bfc3535f7b9731e5d6b6cc6d
MaxwellJChen <maxwelljchen1@gmail.com>
2025-04-06 08:51:32 -0500

    finished preliminary about page

---
+++ scripts/about.js
@@ -0,0 +1,28 @@
+// const fade_classes = ["timeline-text", "timeline-middle", "timeline-date"];
+const faders = [...document.getElementsByClassName("timeline")];
+// for(let i = 0; i < fade_classes.length; i++) {
+//     const cur = document.getElementsByClassName(fade_classes[i]);
+//     faders.push(...cur);
+// }
+
+// console.log(faders);
+
+const appear_options = {
+    threshold: 1,
+    rootMargin: "0px 0px -200px 0px"
+};
+const appear_on_scroll = new IntersectionObserver(function(entries, appear_on_scroll) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        }
+        else {
+            entry.target.classList.add("appear");
+            appear_on_scroll.unobserve(entry.target);
+        }
+    })
+}, appear_options);
+
+faders.forEach(fader => {
+    appear_on_scroll.observe(fader);
+});
