8ef93247db1e8eb80216dabca734f8b319b3eadb
MaxwellJChen <maxwelljchen1@gmail.com>
2025-04-06 01:42:51 -0500

    experimented with dynamic resizing, but struggles with resizing while succeeding on load; does not read page dimensions properly for large resizes

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,12 @@ const ids = ["cpu", "motherboard", "monitor", "keyboard", "ram", "ssd", "gpu"];
 function update() {
     width = window.innerWidth;
     height = window.innerHeight;
-
     let new_rotated = width <= 0.9 * height; 
     if(new_rotated != rotated) {
         rotated = new_rotated;
         rotate();
     }
+    // requestAnimationFrame(resize);
 }
 
 function rotate() {
@@ -21,14 +21,41 @@ function rotate() {
     for(let i = 0; i < images.length; i++) {
         let element = document.getElementById(ids[i]);
         if(!element) {
-            let container = document.getElementsByClassName("image-container");
+            let container = document.getElementById("image-container");
             element = document.createElement("img");
             element.id = ids[i];
-            container[0].appendChild(element);
+            container.appendChild(element);
         }
         element.src = path + images[i] + ".png";
     }
 }
 
-window.addEventListener('load', update);
-window.addEventListener('resize', update);
+function resize() {
+    console.log(window.getComputedStyle(document.body).getPropertyValue("--grid-dim"));
+
+    // get height space
+    const container = document.getElementById("image-container");
+    const rect = container.getBoundingClientRect();
+    const space = height - rect.top;
+    const margin = 80;
+    
+    // get result
+    const height_space = (space - margin) / (rotated ? 19 : 8);
+    const width_space = (width - margin) / (rotated ? 8 : 19);
+    const res = Math.min(height_space, width_space, 50);
+
+    // set value
+    document.documentElement.style.setProperty("--grid-dim", `${res}px`); 
+
+    console.log(window.getComputedStyle(document.body).getPropertyValue("--grid-dim"));
+    console.log(`${height} ${width}`);
+}
+
+window.addEventListener("load", () => {
+    // console.log("load");
+    update();
+});
+window.addEventListener("resize", () => {
+    // console.log("resize");
+    requestAnimationFrame(update);
+});
